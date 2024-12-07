1643c58144f0b4d1862d394e2ed62743cce9d44e
👻👽🤖🧙‍♂️🫠 <sprain6628@gmail.com>
2024-12-07 20:16:53 +0900

    feat: create stream from array with index range

--- src/main/java/stream/CreateStreamFromArray.java
+++ src/main/java/stream/CreateStreamFromArray.java
@@ -8,5 +8,10 @@ public class CreateStreamFromArray {
         // 어레이도 스트림으로 만들 수 있다
         String[] arr = new String[] { "a", "b", "c" };
         Stream<String> stream = Arrays.stream(arr);
+
+        // 인덱스로 어레이의 일부만 스트림으로 만들 수도 있다.
+        Stream<String> streamOfArrayPart = Arrays.stream(arr, 1, 3);
+        streamOfArrayPart.forEach(System.out::println); // 메소드 레퍼런스
+        // 1번부터 2번 인덱스 즉, b, c만 출력됨
     }
 }
